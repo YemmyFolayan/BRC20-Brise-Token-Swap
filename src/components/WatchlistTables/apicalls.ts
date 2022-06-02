b7b07753cfeb5b614b3e55cf668f20c632fb6aac
ethabhijit <coder.abhijit90@gmail.com>
2022-06-02 16:44:39 +0530

    update: headers of api call

--- src/components/WatchlistTables/apicalls.ts
+++ src/components/WatchlistTables/apicalls.ts
@@ -1,15 +1,20 @@
-
 const API = `https://api.nomics.com/v1/currencies/ticker?key=dd407177db9f965ba2824dfd7f7840acf4a5949f&interval=1d,30d&convert=INR&platform-currency=BRISE2&per-page=100&page=1`
 
 // get token data
 const getTokens = () => {
-  return fetch(`${API}`, {
-    method: "GET"
-  })
-    .then(response => {
-      return response.json();
-    })
-    .catch(err => console.log(err));
-};
+  let headersList = {
+    Accept: '*/*',
+    'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+  }
 
-export default getTokens;
+  return fetch(`${API}`, {
+    method: 'GET',
+    headers: headersList
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => console.log(err))
+}
+
+export default getTokens
