9717969fe888788025a1ffa59c993cde63e32db0
ethabhijit <coder.abhijit90@gmail.com>
2022-06-17 18:37:56 +0530

    update: reward & router address

--- src/constants/abis/reward.ts
+++ src/constants/abis/reward.ts
@@ -2,7 +2,7 @@ import { Interface } from '@ethersproject/abi'
 import { abi as REWARD_ABI } from './reward.json'
 
 const REWARD_INTERFACE = new Interface(REWARD_ABI)
-const REWARD_ADDRESS = '0xC2e7bB3dADbF79A8A6463000F7967cCe51BF1174'
+const REWARD_ADDRESS = '0xeC4040490337b2D553dCC42BDBD0eD8da378F07f'
 
 export default REWARD_INTERFACE
 export { REWARD_ABI, REWARD_ADDRESS }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@evofinance9/sdk'
 import { abi as ROUTER_ABI } from "./abis/router.json";
 
-export const ROUTER_ADDRESS = '0x0594907683c45F8F7BF8cCbAC7985B14dC2C8E25'
+export const ROUTER_ADDRESS = '0x66E20e2aA7905d19EF3270dDd3f27B0eF7b5e339'
 export { ROUTER_ABI }
 
 // a list of tokens by chain
