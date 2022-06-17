275b9f09ac08339a5703a944fc928210dbf9cb80
ethabhijit <coder.abhijit90@gmail.com>
2022-06-17 09:00:46 +0530

    fix: data in nomics api

--- src/components/WatchlistTables/apicalls.ts
+++ src/components/WatchlistTables/apicalls.ts
@@ -1,4 +1,4 @@
-const API = `https://api.nomics.com/v1/currencies/ticker?key=9c34b8d924af87a41651ef42956ceba1&interval=1d,30d&convert=INR&platform-currency=BRISE2&per-page=100&page=1`
+const API = `https://api.nomics.com/v1/currencies/ticker?key=9c34b8d924af87a41651ef42956ceba1&interval=1d,30d&convert=USD&platform-currency=BRISE2&per-page=100&page=1`
 
 // get token data
 const getTokens = () => {
