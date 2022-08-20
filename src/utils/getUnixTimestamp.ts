6a2fa3a1827acdf547b017b10c4f05d4a4f8ed93
Arundhati <arundhati.gunu@gmail.com>
2022-08-20 17:21:42 +0530

    UTC time changes

--- src/utils/getUnixTimestamp.ts
+++ src/utils/getUnixTimestamp.ts
@@ -4,12 +4,12 @@ import { Contract } from 'ethers'
 import moment from 'moment'
 
 export default async function getUnixTimestamp(contract: Contract | null, datetime) {
-  const year = moment(datetime).format('YYYY')
-  const month = moment(datetime).format('M')
-  const day = moment(datetime).format('D')
-  const hour = moment(datetime).format('H')
-  const minute = moment(datetime).format('M')
-  const second = moment(datetime).format('S')
+  const year = moment(datetime).utc().format('YYYY')
+  const month = moment(datetime).utc().format('M')
+  const day = moment(datetime).utc().format('D')
+  const hour = moment(datetime).utc().format('H')
+  const minute = moment(datetime).utc().format('M')
+  const second = moment(datetime).utc().format('S')
 
   if (contract) {
     const timestamp = await contract?.callStatic['toTimestamp(uint16,uint8,uint8,uint8,uint8,uint8)'](
