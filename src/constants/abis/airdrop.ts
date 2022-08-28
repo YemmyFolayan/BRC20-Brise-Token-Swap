c914ae0ecb5be02197f302c04179ea7e6367a70a
ethabhijit <coder.abhijit90@gmail.com>
2022-08-28 16:32:11 +0530

    update: address to live

--- src/constants/abis/airdrop.ts
+++ src/constants/abis/airdrop.ts
@@ -2,7 +2,7 @@ import { Interface } from '@ethersproject/abi'
 import { abi as AIRDROP_ABI } from './airdrop.json'
 
 const AIRDROP_INTERFACE = new Interface(AIRDROP_ABI)
-const AIRDROP_ADDRESS = '0x3d65a393D5F2ea7D3507ba9E666C7CA8253078E3'
+const AIRDROP_ADDRESS = '0x0dE2fA5ac7ef946B870d62681B01B340Af08a28D'
 
 export default AIRDROP_INTERFACE
 export { AIRDROP_ABI, AIRDROP_ADDRESS }
--- src/constants/abis/lock.ts
+++ src/constants/abis/lock.ts
@@ -2,7 +2,7 @@ import { Interface } from '@ethersproject/abi'
 import { abi as LOCK_ABI } from './lock.json'
 
 const LOCK_INTERFACE = new Interface(LOCK_ABI)
-const LOCK_ADDRESS = '0x2C529D33859BfB10b9D06199AE5681B866dD8e00'
+const LOCK_ADDRESS = '0x69215BFA299E1dd0C8A7Ae9e090D5a6F6aCB3122'
 
 export default LOCK_INTERFACE
 export { LOCK_ABI, LOCK_ADDRESS }
